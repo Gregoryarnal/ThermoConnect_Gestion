4a2d2106fd3d33897eb95a406d153eef4ff47105
Gregoryarnal <Gregoryarnal@users.noreply.github.com>
2022-01-11 08:15:53 +0000

    ✅ Ready to clone and code.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""Python setup.py for project_name package"""
+"""Python setup.py for thermoconnect_gestion package"""
 import io
 import os
 from setuptools import find_packages, setup
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 def read(*paths, **kwargs):
     """Read the contents of a text file safely.
-    >>> read("project_name", "VERSION")
+    >>> read("thermoconnect_gestion", "VERSION")
     '0.1.0'
     >>> read("README.md")
     ...
@@ -30,17 +30,17 @@ def read_requirements(path):
 
 
 setup(
-    name="project_name",
-    version=read("project_name", "VERSION"),
-    description="project_description",
-    url="https://github.com/author_name/project_urlname/",
+    name="thermoconnect_gestion",
+    version=read("thermoconnect_gestion", "VERSION"),
+    description="Awesome thermoconnect_gestion created by Gregoryarnal",
+    url="https://github.com/Gregoryarnal/ThermoConnect_Gestion/",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
-    author="author_name",
+    author="Gregoryarnal",
     packages=find_packages(exclude=["tests", ".github"]),
     install_requires=read_requirements("requirements.txt"),
     entry_points={
-        "console_scripts": ["project_name = project_name.__main__:main"]
+        "console_scripts": ["thermoconnect_gestion = thermoconnect_gestion.__main__:main"]
     },
     extras_require={"test": read_requirements("requirements-test.txt")},
 )
